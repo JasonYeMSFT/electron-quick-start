082025d2cac7a2b139c1906ec23e507f1c6e7635
Jason Ye <t-chuye@microsoft.com>
2018-06-19 09:14:48 -0700

    add dialog

--- main.js
+++ main.js
@@ -27,7 +27,9 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -48,3 +50,5 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {dialog} = require('electron').remote
+
+function openDialog () {
+    console.log(dialog.showErrorBox("title", "content"))
+}
+
+document.querySelector("#bt").addEventListener('click', openDialog)
